9412458de9e512276007a6fb1b54f0d3bf2b6fda
Manuel Barroso Pecellín <manubp1997@gmail.com>
2021-09-03 11:52:59 +0200

    Creación rama develop y subida de cambios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,17 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ProductoComponent } from './pages/producto/producto.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    PortafolioComponent
+    PortafolioComponent,
+    AboutComponent,
+    ProductoComponent
   ],
   imports: [
     BrowserModule
